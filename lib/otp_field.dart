5c4fa226bbd97350411d805e52d0c60bf9bf7a17
sagar-sachdev-sc <sagar-sachdev-sc>
2020-09-11 16:49:45 +0530

    updated

--- lib/otp_field.dart
+++ lib/otp_field.dart
@@ -107,7 +107,7 @@ class _OTPTextFieldState extends State<OTPTextField> {
       height: 50.0,
       child: Stack(children: <Widget>[
         Padding(
-          padding: const EdgeInsets.only(left:1.5),
+          padding: const EdgeInsets.only(left:1.7),
           child: Image.asset('assets/images/ffoutline.png'),
         ),
         TextField(
