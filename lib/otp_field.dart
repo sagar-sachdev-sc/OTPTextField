9675be2cd9d87001c6dfdba7a5ac66e5907de659
sagar-sachdev-sc <sagar-sachdev-sc>
2020-09-11 14:47:00 +0530

    updated

--- lib/otp_field.dart
+++ lib/otp_field.dart
@@ -116,9 +116,9 @@ class _OTPTextFieldState extends State<OTPTextField> {
             counterText: "",
             border: widget.fieldStyle == FieldStyle.image
                 ? OutlineInputBorder(
-                    borderSide: BorderSide(width: 10.0, color: Colors.orange))
+                    borderSide: BorderSide(width: 100.0, color: Colors.orange))
                 : OutlineInputBorder(
-                    borderSide: BorderSide(width: 10.0, color: Colors.orange))),
+                    borderSide: BorderSide(width: 100.0, color: Colors.orange))),
         onChanged: (String str) {
           // Check if the current value at this position is empty
           // If it is move focus to previous text field.
